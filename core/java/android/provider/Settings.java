781270d363137e926f4e43e1c7b905a87831f3ee
beanstown106 <nbenis106@gmail.com>
2016-01-17 09:14:19 -0500

    Long-press power while display is off for torch
    Squash of:
    
    Author: beanstown106 <nbenis106@gmail.com>
    Date:   Sun Jan 17 09:14:19 2016 -0500
        policy: Long-press power while display is off for torch
        Long-press the power button while the display is off
        to turn the torchlight on and off.
        Credits:
         - Lion0738: The main hooks here:
           https://github.com/lion0738/android_frameworks_base/commit/9af2b7844a4d973c8b6c542d7937f56a24a7e5f1
         - Atlantis: The logic on where to hook into this for power button only
         - Alex Cruz: Helping and giving me some pointers along the way
        Change-Id: I14365389990eb06daaa127f5db66df45abf6c064
        (cherry picked from commit 604f00ce176463bc3e60b99195fc7a757f49f957)
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date: Sat Dec 24 13:22:53 2016 -0800
        [1/3] Torch long press power: add auto-off function
        Change-Id: Icdf50082324f8292859f0df8b271e730b02c84e7
    Change-Id: I1279646625770be20f70286cc421f20d8a8b8b9f
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4822,6 +4822,16 @@ public final class Settings {
         /**
          * @hide
          */
+        public static final String TORCH_LONG_PRESS_POWER_GESTURE = "torch_long_press_power_gesture";
+
+        /**
+         * @hide
+         */
+        public static final String TORCH_LONG_PRESS_POWER_TIMEOUT = "torch_long_press_power_timeout";
+
+        /**
+         * @hide
+         */
         public static final String VOLUME_BUTTON_MUSIC_CONTROL = "volume_button_music_control";
 
         /**
@@ -5139,6 +5149,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_ADJUSTMENT);
             PRIVATE_SETTINGS.add(DISPLAY_PICTURE_ADJUSTMENT);
             PRIVATE_SETTINGS.add(LIVE_DISPLAY_HINTED);
+            PRIVATE_SETTINGS.add(TORCH_LONG_PRESS_POWER_GESTURE);
+            PRIVATE_SETTINGS.add(TORCH_LONG_PRESS_POWER_TIMEOUT);
         }
 
         /**
